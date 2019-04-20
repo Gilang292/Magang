dd64090673dee70573fb90e03031d00801aaaa42
Gilang Adi <novalinobramasta1@gmail.com>
2019-04-21 02:20:21 +0700

    perubahan routing baru

--- resources/views/gilang1.blade.php
+++ resources/views/gilang1.blade.php
@@ -5,6 +5,6 @@
     <title></title>
   </head>
   <body>
-    Gilang Adi Saputra1
+    Gilang292
   </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,7 @@
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('/masuk', function(){
+
+Route::get('/coba', function () {
     return view('gilang1');
 });
